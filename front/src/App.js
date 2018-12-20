674edff8af031c493921ae2378d2c8cd53bee557
Anais Miltenberger <anaismiltenberger@gmail.com>
2018-12-20 14:21:39 +0100

    ajout du footer

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import ModalExample from '../src/components/PopUp';
 import Navbar from './components/Navbar';
 import MeteoWidget from './components/MeteoWidget';
 import Video from './components/Video';
+import Footer from './components/Footer';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ModalExample />
         <Navbar />
         <Video />
         <MeteoWidget />
+        <Footer />
       </div>
     );
   }
