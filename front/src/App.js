254697d4af0197f9743c0fbc847180000cb4cccb
MickaelMuller <Muller-Mickael@live.fr>
2018-12-20 14:14:06 +0100

    route + dossier

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import ModalExample from '../src/components/PopUp';
-import Navbar from './components/Navbar';
-import MeteoWidget from './components/MeteoWidget';
-import Video from './components/Video';
-
+import { Route, Switch } from 'react-router-dom';
+import Home from './components/home/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ModalExample />
-        <Navbar />
-        <Video />
-        <MeteoWidget />
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
       </div>
     );
   }
