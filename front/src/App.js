d31b17e029cbbcd40a49d14a471d080dc8b7f8fb
clembesso <dessuane@hotmail.fr>
2018-12-20 16:53:28 +0100

    mise à jours des cards et titres

--- front/src/App.js
+++ front/src/App.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import Footer from './components/Footer';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/home/Home';
-
+import Sensibilisation from './components/Sensibilisation.jsx'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-       <Footer />
+        <Sensibilisation />
       </div>
     );
   }
