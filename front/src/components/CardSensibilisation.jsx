18c4985ffdc06e54f847a15c8828ef5d628b2391
Jose Perez <jmpo@rocketmail.com>
2018-12-20 21:37:38 +0100

    completion du .json pour les cards et animation des hovers

--- front/src/components/CardSensibilisation.jsx
+++ front/src/components/CardSensibilisation.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardTitle, CardText, CardImg, CardImgOverlay } from 'reactstrap';
 import './CardSensibilisation.scss';
+
 class CardSensibilisation extends Component {
   constructor(props) {
     super(props);
@@ -10,7 +11,7 @@ class CardSensibilisation extends Component {
     return (
       <div className="CardSensibilisation">
           <Card inverse className="card-sensib">
-            <CardImg width="110%" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx1txhKdIR9zcqgMBs4Py9hqZXsBY0kHmNWHZi5wdguKVmfIYSMA" alt="Card image cap" className="card-image"/>
+            <CardImg width="110%" src={this.props.image} alt="Card image cap" className="card-image"/>
             <CardImgOverlay>
               <CardTitle className="card-title">{this.props.name}</CardTitle>
               <CardText className="card-text">{this.props.description}</CardText>
