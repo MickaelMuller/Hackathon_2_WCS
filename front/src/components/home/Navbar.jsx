3574f58b8e141607fd5668100e7b7c672950d096
Lucie Genest <luciegenest@hotmail.fr>
2018-12-20 15:42:22 +0100

    popup onclick of navbar

--- front/src/components/home/Navbar.jsx
+++ front/src/components/home/Navbar.jsx
@@ -1,16 +1,43 @@
-import React from 'react';
-import {Container, Row} from 'reactstrap';
+import React, { Component } from 'react';
+import { Container, Row } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './Navbar.scss';
 
-const Navbar = () => {
-    return ( 
-        <Container fluid className='Navbar'>
-            <div className="nyt-navbar" />
-            <Row className='red'>
-                <h3>FLASH INFO</h3>
-            </Row>
-        </Container>
-     );
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    }
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    return (
+      <Container fluid className='Navbar'>
+        <div className="nyt-navbar" onClick={this.toggle} />
+        <Row className='red'>
+          <h3>FLASH INFO</h3>
+        </Row>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>Vous ne souhaitez pas en arriver là ?</ModalHeader>
+          <ModalBody className="text">
+            Vous voulez changer les choses ? Nous aussi !
+          </ModalBody>
+          <ModalFooter>
+            <Button color="success" onClick={this.toggle}>Les solutions ici =></Button>
+          </ModalFooter>
+        </Modal>
+      </Container>
+    );
+  }
 }
- 
+
 export default Navbar;
