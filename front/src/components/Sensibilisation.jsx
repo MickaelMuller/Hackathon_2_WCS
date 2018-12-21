4fca4d15b8eaee6698f66e5bda33cd4c0be5deb0
Anais Miltenberger <anaismiltenberger@gmail.com>
2018-12-21 09:39:30 +0100

    margin pages monde et lien vers site the shift project

--- front/src/components/Sensibilisation.jsx
+++ front/src/components/Sensibilisation.jsx
@@ -22,7 +22,9 @@ class Sensibilistation extends Component {
     return (
       <div className="Sensibilisation">
         <div className="header">
-          <img src="/medias/theshiftproject.png" alt="theshiftproject" className="margin" />
+          <a target="_blank" href="https://theshiftproject.org/" title="The Shift Project">
+            <img src="/medias/theshiftproject.png" alt="theshiftproject" className="margin" />
+          </a>
           <p class="line-1 anim-typewriter">"C’est à condition d’accepter les limites de </p>
           <p class="line-1 anim2-typewriter2">ce monde qu’une politique salubre sera possible."</p>
           <p class="line-1 anim3-typewriter3 textsize">Matthieu Auzanneau (Directeur de The Shift Project)</p>
@@ -53,11 +55,11 @@ class Sensibilistation extends Component {
         </Container>
         <p className="text-left">Retour au scénario <br />catastrophe(2025)</p>
         <p className="text-right">Aller au scénario<br />sobriété (2025)</p>
-        <Button className="joel-left" color="success"><Link style={{color:"white"}} to="/"><i className=" fa fa-arrow-circle-o-left fa-3x"></i></Link></Button>
-        <Button className="joel" color="danger"><Link style={{color:"white"}} to="/happiness"><i className=" fa fa-arrow-circle-o-right fa-3x"></i></Link></Button>
-    </div>
-        );
-      }
-    }
-    
+        <Button className="joel-left" color="success"><Link style={{ color: "white" }} to="/"><i className=" fa fa-arrow-circle-o-left fa-3x"></i></Link></Button>
+        <Button className="joel" color="danger"><Link style={{ color: "white" }} to="/happiness"><i className=" fa fa-arrow-circle-o-right fa-3x"></i></Link></Button>
+      </div>
+    );
+  }
+}
+
 export default Sensibilistation;
